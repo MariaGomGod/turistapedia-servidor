403157cb50126cfd3cd43c6604e5b42849d58082
Maria Gomez <maria_gg86@yahoo.es>
2021-05-31 21:16:59 +0200

    filtro de accesibilidad finalizado

--- routes/pointsOfInterest.js
+++ routes/pointsOfInterest.js
@@ -7,8 +7,32 @@ router.get("/", (req, res) => {
 
     /* MongoDB hace consultas mediante ejemplos. En este caso le estamos pidiendo que nos devuelva documentos similares al que le ofrecemos
     como primer argumento. */
+    const categories = req.query.categories.split(",");
+    const accessible = req.query.accessible.split(",");
 
-    PointOfInterest.find({ categories: {"$in": req.query.categories.split(",")}, active: true }).exec((error, pointsOfInterest) => { 
+    /* Restrinjo la búsqueda a aquellos documentos que cumplan con los filtros de accesibilidad, es decir, cuyo nombre esté presente
+    en el array "accessible". */
+    const accessibleFilter = {};
+
+    // Solo incluimos aquellos filtros que están contenidos en el array "accessible".
+    if (accessible.includes("adaptedAccess")) {
+        accessibleFilter["accessible.adaptedAccess"] = true;
+    }
+    if (accessible.includes("adaptedToilet")) {
+        accessibleFilter["accessible.adaptedToilet"] = true;
+    }
+    if (accessible.includes("adaptedRoom")) {
+        accessibleFilter["accessible.adaptedRoom"] = true;
+    }
+    if (accessible.includes("audioGuide")) {
+        accessibleFilter["accessible.audioGuide"] = true;
+    }
+
+    PointOfInterest.find({
+        categories: { "$in": categories },
+        ... accessibleFilter,
+        active: true
+    }).exec((error, pointsOfInterest) => {
 
         /* La función find devuelve un objeto de tipo Query. La misma no se ejecutará hasta que no llamemos a su función exec. */
 
@@ -62,20 +86,20 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req,res) => {
+router.put("/:id", (req, res) => {
     const id = req.params.id;
     const body = ramda.pick(["name", "description", "links", "categories", "photos", "review", "latitude", "logitude", "accessible", "address", "active"], req.body);
     // Equivalente a const body = { req.body.name, req.body.description, req.body.links, etcétera }
 
     PointOfInterest.findByIdAndUpdate(
-        id, 
-        body, 
+        id,
+        body,
         { new: true, runValidators: true, context: "query" }, // options
         (error, updatePointOfInterest) => {
             if (error) {
                 res.status(400).json({ ok: false, error });
             } else if (!updatePointOfInterest) {
-                res.status(404).json({ ok: false, error: "Point of interest not found"} );
+                res.status(404).json({ ok: false, error: "Point of interest not found" });
             } else {
                 res.status(200).json({ ok: true, updatePointOfInterest });
             }
@@ -83,18 +107,18 @@ router.put("/:id", (req,res) => {
     );
 });
 
-router.put("/:id/publish", (req,res) => {
+router.put("/:id/publish", (req, res) => {
     const id = req.params.id;
-    
+
     PointOfInterest.findByIdAndUpdate(
-        id, 
-        { $set: { active: true } }, 
+        id,
+        { $set: { active: true } },
         { new: true, runValidators: true, context: "query" }, // options
         (error, updatePointOfInterest) => {
             if (error) {
                 res.status(400).json({ ok: false, error });
             } else if (!updatePointOfInterest) {
-                res.status(404).json({ ok: false, error: "Point of interest not found"} );
+                res.status(404).json({ ok: false, error: "Point of interest not found" });
             } else {
                 res.status(200).json({ ok: true, updatePointOfInterest });
             }
@@ -102,18 +126,18 @@ router.put("/:id/publish", (req,res) => {
     );
 });
 
-router.delete("/:id", (req,res) => {
+router.delete("/:id", (req, res) => {
     const id = req.params.id;
 
     PointOfInterest.findByIdAndUpdate(
-        id, 
+        id,
         { $set: { active: false } }, // borrado lógico (no borramos realmente el documento, sino que no le aparece al usuario)
         { new: true, runValidators: true, context: "query" }, // options
         (error, updatePointOfInterest) => {
             if (error) {
                 res.status(400).json({ ok: false, error });
             } else if (!updatePointOfInterest) {
-                res.status(404).json({ ok: false, error: "Point of interest not found"} );
+                res.status(404).json({ ok: false, error: "Point of interest not found" });
             } else {
                 res.status(200).json({ ok: true, updatePointOfInterest });
             }
